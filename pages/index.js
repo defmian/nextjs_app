cdb424f31d4ee82e6320e1c9e17460ba877d4ac1
Damian Dzialo <ddzialo28@interia.pl>
2021-02-15 23:42:57 +0100

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,4 @@
-import Head from 'next/head'
+import Head from "next/head";
 
 export default function Home() {
   return (
@@ -54,7 +54,7 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
         </a>
       </footer>
@@ -194,16 +194,15 @@ export default function Home() {
         html,
         body {
           padding: 0;
-          margin: 0;
+          margin: 10px;
           font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
             Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
             sans-serif;
         }
-
         * {
           box-sizing: border-box;
         }
       `}</style>
     </div>
-  )
+  );
 }
