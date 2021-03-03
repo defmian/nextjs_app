1521f00eab242215e11f55fb5ceeb9bfb148a72f
Damian Dzialo <ddzialo28@interia.pl>
2021-03-03 23:58:23 +0100

    Styling layout and header

--- pages/_app.js
+++ pages/_app.js
@@ -5,21 +5,25 @@ import "../styles/global.css";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="mx-auto w-9/12 my-8">
+      <header>
+        <h1 className="text-6xl font-bold text-center">My Blog</h1>
+        <nav className="my-4">
+          <ul className="flex flex-row justify-center space-x-4">
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
       <Component {...pageProps} />
     </div>
   );
