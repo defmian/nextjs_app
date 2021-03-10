9515af18acde964877271d470c9f916fc784e206
Damian Dzialo <ddzialo28@interia.pl>
2021-03-10 23:32:28 +0100

    Implement excerpt function

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from "next/head";
-import { blogPosts } from "../lib/data";
+// import { blogPosts } from "../lib/data";
 import { request } from "../lib/datocms";
 import App from "next/head";
 import Link from "next/link";
 import { getAllPosts } from "../lib/datocms";
+import { Image } from "next/image";
 
-export default function Home({ data }) {
+export default function Home({ allPosts }) {
   return (
     <div className="container">
       <Head>
@@ -13,40 +14,45 @@ export default function Home({ data }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* <main>
-        <h1 className="">
+      <main>
+        <h1 className="my-5">
           Welcome to my new blog about new technologies and programming
-          languages. I hope you will enjoy reading and you find something for yourself.
+          languages. I hope you will enjoy reading and you find something for
+          yourself.
         </h1>
+
         <div>
-          {blogPosts.map((post) => (
+          {allPosts.map((post) => (
             <BlogItem key={post.slug} {...post} />
           ))}
         </div>
-      </main> */}
+      </main>
     </div>
   );
 }
 
-function BlogItem({ slug, title, content, date }) {
+function BlogItem({ slug, header, paragraph, date }) {
   return (
     <div className="border-gray-200 rounded-lg border-2 shadow-sm p-4 mb-8">
       <div className="font-bold text-2xl ">
         <Link href={`/blog/${slug}`}>
-          <a className="text-gray-800">{title}</a>
+          <a className="text-gray-800">{header}</a>
         </Link>
       </div>
-      <div className="text-xs inline font-light antialiased ">
-        {date.toDateString()}
-      </div>
-      <div className="">{content}</div>
+      <div className="text-sm inline font-light antialiased">{date}</div>
+      <div className="">{excerptPostContent(paragraph)}</div>
     </div>
   );
 }
+
+function excerptPostContent(paragraph) {
+  return paragraph.length > 200
+    ? paragraph.substring(0, 200) + "..."
+    : paragraph;
+}
+
 export async function getServerSideProps({ preview = false }) {
   const allPosts = (await getAllPosts(preview)) || [];
-
-  console.log({ allPosts });
   return {
     props: { allPosts },
   };
