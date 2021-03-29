604f24656d619647db58aa4da863c12ab2b3d8bd
Damian Dzialo <ddzialo28@interia.pl>
2021-03-29 23:34:12 +0200

    Fix error with fetching data from cms

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,40 +1,49 @@
 import Head from "next/head";
-import { blogPosts } from "../../lib/data";
-import { getBlogPost } from "../../lib/datocms";
+import { getBlogPost, getAllPostsWithSlug } from "../../lib/datocms";
 
 // pages/blog/[slug].js
-export default function BlogPost({ title, date, content }) {
+export default function BlogPost({ post }) {
+  console.log(post);
   return (
     <div className="container">
       <Head>
-        <title>{title}</title>
+        <title>{post.header}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1>{title}</h1>
-        <div>{date}</div>
-        <div>{content}</div>
+        <h1>{post.header}</h1>
+        <div>{post.date}</div>
+        <div>{post.paragraph}</div>
+        <div className="bg-gray-500">
+          <p>Test czy to w ogole dzialooo</p>
+        </div>
       </main>
     </div>
   );
 }
 
 // This function gets called at build time
+// it needs to define a list of paths that have to be rendered to html at build time
 export async function getStaticPaths() {
   // Get the paths we want to pre-render based on post
+  const allPosts = await getAllPostsWithSlug();
   return {
-    paths: getBlogPost.map((item) => ({
-      params: {
-        slug: item.slug,
-      },
-    })),
+    paths: allPosts?.map((post) => `/blog/${post.slug}`) || [],
     fallback: false,
   };
 }
-export async function getStaticProps(context) {
-  const { params } = context;
+
+export async function getStaticProps({ params, preview = false }) {
+  const data = await getBlogPost();
+
   return {
-    props: getBlogPost.find((item) => item.slug === params.slug), // will be passed to the page component as props
+    props: {
+      preview,
+      post: {
+        ...data.find((item) => item.slug === params.slug),
+      },
+      morePosts: data?.morePosts ?? [],
+    },
   };
 }
